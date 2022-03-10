bdb41697fb0206b1fd0c344dbdd2e2fb1aab8f0a
Nandha Kumar <nandhaleep@gmail.com>
2022-03-10 16:33:27 +0530

    add the funtion modified

--- div.js
+++ div.js
@@ -1,5 +1,8 @@
-var a
-var b
+funtion div()
+{
+var a= document.getElementById("num1").value
+var b= document.getElementById("num2").value
 var result
 result= a/b
-
+document.getElementById("result").value = result
+}
